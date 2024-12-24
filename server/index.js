bc3f0e164d08d0760240028955403cb17d65803f
alvisfdo <ansker.berchmans@gmail.com>
2024-12-25 00:57:06 +0530

    incoming cros updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:3000"],
+        origin: ["http://localhost:5173", "http://localhost:3000", "http://13.53.37.199"],
         methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
     }),
